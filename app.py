6452ef3c425c7579b9478caa647cebb1df44e901
leandro lencina <128265950+leadministrador@users.noreply.github.com>
2026-08-17 12:24:29 -0300

    Admin por usuario, sin clave en el link

--- app.py
+++ app.py
@@ -4851,4 +4851,3 @@ if os.getenv("AVISOS_AUTOMATICOS", "1") == "1":
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=int(os.getenv("PORT","5000")),debug=os.getenv("FLASK_DEBUG","0")=="1")
-    
